57e950124804aade0170a45b62160fdee1a48c15
Arrow <arrow.swiech@gmail.com>
2023-05-22 23:57:52 +0200

    10_Commentless

--- 10_Listener/src/ColorButton.java
+++ 10_Listener/src/ColorButton.java
@@ -4,45 +4,42 @@ import java.awt.event.*;
 
 public class ColorButton extends JFrame implements ActionListener {
 
-    // Define an array of colors
-    private Color[] colors = {Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.CYAN};
-    // Define a variable to keep track of the current color index
+    private Color[] colors = { Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.CYAN };
+
     private int colorIndex = 0;
-    // Define a button
+
     private JButton button;
 
     public ColorButton() {
-        // Set the title of the frame
+
         super("Color Button");
-        // Set the size of the frame
+
         setSize(300, 200);
-        // Set the layout of the frame to null
+
         setLayout(null);
-        // Create a button with text "Click"
+
         button = new JButton("Click");
-        // Set the bounds of the button
+
         button.setBounds(100, 75, 100, 50);
-        // Add an action listener to the button
+
         button.addActionListener(this);
-        // Add the button to the frame
+
         add(button);
-        // Set the default close operation of the frame
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Set the visibility of the frame to true
+
         setVisible(true);
     }
 
-
-    // Override the actionPerformed method of ActionListener interface
     @Override
     public void actionPerformed(ActionEvent e) {
-        // If the source of the event is the button
+
         if (e.getSource() == button) {
-            // Set the background color of the button to the current color in the array
+
             button.setBackground(colors[colorIndex]);
-            // Increment the color index by one
+
             colorIndex++;
-            // If the color index reaches the length of the array, reset it to zero
+
             if (colorIndex == colors.length) {
                 colorIndex = 0;
             }
@@ -50,7 +47,7 @@ public class ColorButton extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
-        // Create an instance of ColorButton class
+
         new ColorButton();
     }
 }
